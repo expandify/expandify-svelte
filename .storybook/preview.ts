63b9f2d83be946cb6a41f9c1791b723b6cc0ce0b
Till Wittenburg <wittenburg.till+git@gmail.com>
2024-12-13 20:42:21 +0100

    feat: add storybook

---
+++ .storybook/preview.ts
@@ -0,0 +1,28 @@
+import type { Preview } from '@storybook/svelte';
+import '../src/app.css';
+
+const preview: Preview = {
+	parameters: {
+		controls: {
+			matchers: {
+				color: /(background|color)$/i,
+				date: /Date$/i
+			}
+		}
+	}
+};
+
+export default preview;
+import { withThemeByClassName } from '@storybook/addon-themes';
+
+/* snipped for brevity */
+
+export const decorators = [
+	withThemeByClassName({
+		themes: {
+			light: 'light',
+			dark: 'dark',
+		},
+		defaultTheme: 'light',
+	}),
+];
---
+++ src/lib/components/common/Card.stories.svelte
@@ -0,0 +1,40 @@
+<script module lang="ts">
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+	import Card from './Card.svelte';
+	import type { Album, Artist } from '$lib/types/spotify';
+
+	// More on how to set up stories at: https://storybook.js.org/docs/writing-stories
+	const { Story } = defineMeta({
+		title: 'Example/Card',
+		component: Card,
+		tags: ['autodocs'],
+		argTypes: {
+			card: {
+				control: { type: 'select' },
+				options: [
+					{
+						name: 'Album',
+						artists: [{name: "artist 1"}, {name: "artist 2"}],
+					} as Album
+				]
+			}
+		}
+	});
+
+	const artist: Artist = {
+		name: "Artist 1",
+		external_urls: { spotify: "" },
+		followers: { total: 0, href: "" },
+		genres: [],
+		href: '',
+		id: '',
+		images: [],
+		popularity: 0,
+		type: 'artist',
+		uri: ''
+
+	}
+</script>
+
+<!-- More on writing stories with args: https://storybook.js.org/docs/writing-stories/args -->
+<Story name="Primary" args={{ card: artist }} />
